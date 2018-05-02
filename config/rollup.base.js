607cc3a4787dd71d99cf7628663f32727a323ad1
Yukino Song <syqlds@126.com>
2018-05-02 15:28:12 +0800

    Upgrade to ef-core v0.8.0

--- config/rollup.base.js
+++ config/rollup.base.js
@@ -73,7 +73,7 @@ export default {
 			objedtAssign: 'Object.assign'
 		}),
 		replace({
-			ENV: `'${process.env.NODE_ENV || 'development'}'`
+			'process.env.NODE_ENV': `'${process.env.NODE_ENV || 'development'}'`
 		}),
 		(process.env.NODE_ENV === 'production' && uglify())
 	]
